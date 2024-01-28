5d2f0ec69b6c1ff062379eba369c4ab6c7a85de9
leekwansoo49 <leekwansoo49@gmail.com>
2024-01-28 21:27:38 +0900

    first commit

---
+++ mongodb.py
@@ -0,0 +1,35 @@
+from pymongo import MongoClient
+import os
+
+client = MongoClient("mongodb+srv://admin:james@cluster0.ujzjm.mongodb.net/account?retryWrites=true&w=majority")
+# database and collection code goes here
+db = client.account
+#coll_incomes = db.incomes
+#coll_expenses = db.expenses
+coll_periods = db.periods
+# find code goes here
+# iterate code goes here
+# Close the connection to MongoDB when you're done.
+#client.close()
+
+def insert_period(period, incomes, expenses, comment):
+    data = {"period": period, "incomes": incomes, "expenses": expenses, "comment": comment}
+    print(data)
+    result = coll_periods.insert_many([data])
+    if result:
+        return {"result": result}
+
+
+def fetch_all_periods():
+    """Returns a dict of all periods"""
+    cursor = coll_periods.find()
+    periods = []
+    for doc in cursor:
+       periods.append(doc['period'])
+    return periods
+   
+
+
+def get_period(period):
+    result = coll_periods.find_one({"period": period})
+    return result
